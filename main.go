187945377e6c7f698786c7509505cab7eae01399
John Mullen <john.mullen@hiveio.com>
2019-07-04 12:46:45 -0400

    recreate curl scripts with api

--- main.go
+++ main.go
@@ -1,83 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
 
-//"log"
+//TODO: use cobra for cli
 
 func main() {
 	client := Client{Host: "hive1", Port: 8443}
 	client.Login("admin", "admin", "local")
 
-	pools, _ := client.ListStoragePools()
-	for _, pool := range pools {
-		fmt.Println(pool)
-	}
 	version, _ := client.HostVersion()
 	fmt.Println(version.Version)
 
-	clusters, _ := client.ListClusters()
-	for _, cluster := range clusters {
-		fmt.Println(cluster.String())
+	//storage pools
+	confDir := "/home/john1/work/hiveio/conf"
+	pools := []string{"vms", "iso", "uvs"}
+	for _, pool := range pools {
+		jsonFile, err := os.Open(fmt.Sprintf("%s/pools/%s.conf", confDir, pool))
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer jsonFile.Close()
+		json, _ := ioutil.ReadAll(jsonFile)
+		var sp StoragePool
+		sp.FromJson(json)
+
+		msg, err := client.CreateStoragePool(&sp)
+		fmt.Println(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	confDir := "/work/hiveio/conf/"
-	fmt.Println(confDir)
+	//realm
+	home := Realm{Name: "HOME", FQDN: "home.john-mullen.net", Verified: true}
 
-	// realm, _ := client.GetRealm("default")
-	// fmt.Println(realm)
+	client.CreateRealm(&home)
+	clusters, _ := client.ListClusters()
+	for _, cluster := range clusters {
+		fmt.Println(cluster)
+	}
 
-	// template, _ := client.GetTemplate("kubuntu")
-	// fmt.Println(template)
+	//Default Profile
+	profile := Profile{Name: "default", BypassBroker: true}
+	client.CreateProfile(&profile)
 
-	// profiles, _ := client.ListProfiles()
-	// for _, profile := range profiles {
-	// 	fmt.Println(profile)
-	// }
-	// hosts, _ := client.ListHosts()
-	// for _, host := range hosts {
-	// 	fmt.Printf("%v", host)
-	// }
+	//templates
+	vms, err := client.GetStoragePoolByName("vms")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	files, err := ioutil.ReadDir(fmt.Sprintf("%s/templates/", confDir))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	/*ryzen := StoragePool{ Name: "ryzen",
-	                        Type: "nfs",
-	                        Server: "ryzen",
-	                        Path: "/mnt/files/vms",
-	                        Roles: []string{"template", "guest"},
-	                        MountOptions: []string{"vers=4.1"},
-	                      }
-	  msg, err := client.CreateStoragePool(&ryzen)
-	  fmt.Println(msg)
-	  if err != nil {
-	      log.Fatal(err)
-	  }
-	  pools, err = client.ListStoragePools()
-	  var ryzenId string
-	  for _, pool := range pools {
-	      fmt.Println(pool)
-	      if pool.Name == "ryzen" {
-	          ryzenId = pool.Id
-	      }
-	  }
-	  if ryzenId != "" {
-	      ryzen_copy, err := client.GetStoragePool(ryzenId)
-	      fmt.Printf("ryzen_copy: %v\n", ryzen_copy)
+	for _, f := range files {
+		jsonFile, err := os.Open(fmt.Sprintf("%s/templates/%s", confDir, f.Name()))
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer jsonFile.Close()
+		json, _ := ioutil.ReadAll(jsonFile)
+		var tmpl Template
+		tmpl.FromJson(json)
+		for i := range tmpl.Disks {
+			tmpl.Disks[i].StorageID = vms.ID
+		}
+		msg, err := client.CreateTemplate(&tmpl)
+		fmt.Println(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
-	      fail, err := client.GetStoragePool("test123")
-	      fmt.Println(fail, err)
-
-	      err = client.DeleteStoragePool(ryzenId)
-	      if err != nil {
-	          log.Fatal("Failed to delete ryzen storage pool", err)
-	      }
-	  } else {
-	      log.Print("ryzen pool not found")
-	  }
-	  pools, err = client.ListStoragePools()
-	  for _, pool := range pools {
-	      fmt.Println(pool)
-	  }
-
-	  if err != nil {
-	      log.Fatal(err)
-	  }*/
+	extraHosts := []string{"hive2", "hive3"}
+	for _, host := range extraHosts {
+		ips, err := net.LookupIP(host)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to find ip for %s", host)
+			os.Exit(1)
+		}
+		err = client.JoinHost("admin", "admin", ips[0].String())
+		fmt.Println(err)
+	}
 }
