76760e799640c2f33deeb7c1bc1b459fa68a2ec6
John Mullen <john.mullen@hiveio.com>
2019-07-15 14:47:08 -0400

    fix(storage-create): fix struct

--- cmd/storage-create.go
+++ cmd/storage-create.go
@@ -10,7 +10,7 @@ import (
 )
 
 var storageCreateCmd = &cobra.Command{
-	Use:   "create",
+	Use:   "create [file]",
 	Short: "Add a new storage pool",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
@@ -21,8 +21,8 @@ var storageCreateCmd = &cobra.Command{
 		}
 		defer file.Close()
 		data, _ := ioutil.ReadAll(file)
-		var sp *rest.StoragePool
-		err = unmarshal(data, sp)
+		var sp rest.StoragePool
+		err = unmarshal(data, &sp)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
