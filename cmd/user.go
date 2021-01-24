f2ba4aba907faa0168f86b5a674e2d16e1c47ce5
John Mullen <john.mullen@hiveio.com>
2021-01-24 15:45:44 -0500

    feat(user): add user to rest and hioctl

---
+++ cmd/user.go
@@ -0,0 +1,186 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hive-io/hive-go-client/rest"
+	"github.com/spf13/cobra"
+)
+
+var userCmd = &cobra.Command{
+	Use:   "user",
+	Short: "user operations",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Usage()
+		os.Exit(0)
+	},
+}
+
+var userCreateCmd = &cobra.Command{
+	Use:   "create [file]",
+	Short: "Add a new user",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var file *os.File
+		var err error
+		if args[0] == "-" {
+			file = os.Stdin
+		} else {
+			file, err = os.Open(args[0])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+		defer file.Close()
+		data, _ := ioutil.ReadAll(file)
+		var user rest.User
+		err = unmarshal(data, &user)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		msg, err := user.Create(restClient)
+		fmt.Println(msg)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+var userDeleteCmd = &cobra.Command{
+	Use:   "delete [id]",
+	Short: "delete a user",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		user, err := restClient.GetUser(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = user.Delete(restClient)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+var userDiffCmd = &cobra.Command{
+	Use:   "diff [user1 id] [user2 id]",
+	Short: "compare 2 users",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		user1, err := restClient.GetUser(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		user2, err := restClient.GetUser(args[1])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(cmp.Diff(user1, user2))
+	},
+}
+
+var userGetCmd = &cobra.Command{
+	Use:   "get [id]",
+	Short: "get user details",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		user, err := restClient.GetUser(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(formatString(user))
+	},
+}
+
+var userListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list users",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		bindListFlags(cmd)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		users, err := restClient.ListUsers(listFlagsToQuery())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if cmd.Flags().Changed("details") {
+			fmt.Println(formatString(users))
+		} else {
+			list := []map[string]string{}
+			for _, user := range users {
+				var info = map[string]string{"id": user.ID}
+				if user.Username != "" {
+					info["username"] = user.Username
+				}
+				if user.GroupName != "" {
+					info["groupname"] = user.GroupName
+				}
+				list = append(list, info)
+			}
+			fmt.Println(formatString(list))
+		}
+	},
+}
+
+var userUpdateCmd = &cobra.Command{
+	Use:   "update [file]",
+	Short: "update a user",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var file *os.File
+		var err error
+		if args[0] == "-" {
+			file = os.Stdin
+		} else {
+			file, err = os.Open(args[0])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+		defer file.Close()
+		data, _ := ioutil.ReadAll(file)
+		var user rest.User
+		err = unmarshal(data, &user)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		msg, err := user.Update(restClient)
+		fmt.Println(msg)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(userCmd)
+	userCmd.AddCommand(userCreateCmd)
+
+	userCmd.AddCommand(userDeleteCmd)
+
+	userCmd.AddCommand(userDiffCmd)
+
+	userCmd.AddCommand(userGetCmd)
+
+	userCmd.AddCommand(userListCmd)
+	addListFlags(userListCmd)
+
+	userCmd.AddCommand(userUpdateCmd)
+}
---
+++ rest/user.go
@@ -0,0 +1,105 @@
+package rest
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// UserPreferences
+type UserPreferences struct {
+	UiTimeout float64 `json:"uiTimeout,omitempty"`
+}
+
+// User describes a user record
+type User struct {
+	ID          string           `json:"id"`
+	GroupName   string           `json:"groupName,omitempty"`
+	Password    string           `json:"password,omitempty"`
+	Preferences *UserPreferences `json:"preferences,omitempty"`
+	Realm       string           `json:"realm,omitempty"`
+	Role        string           `json:"role,omitempty"`
+	Tags        []string         `json:"tags,omitempty"`
+	Username    string           `json:"username,omitempty"`
+}
+
+func (user User) String() string {
+	json, _ := json.MarshalIndent(user, "", "  ")
+	return string(json)
+}
+
+// ListUsers returns an array of all users with an optional filter string
+func (client *Client) ListUsers(query string) ([]User, error) {
+	var users []User
+	path := "users"
+	if query != "" {
+		path += "?" + query
+	}
+	body, err := client.request("GET", path, nil)
+	if err != nil {
+		return users, err
+	}
+	err = json.Unmarshal(body, &users)
+	return users, err
+}
+
+// GetUser request a user by id
+func (client *Client) GetUser(id string) (*User, error) {
+	var user *User
+	if id == "" {
+		return user, errors.New("Id cannot be empty")
+	}
+	body, err := client.request("GET", "user/"+id, nil)
+	if err != nil {
+		return user, err
+	}
+	err = json.Unmarshal(body, &user)
+	return user, err
+}
+
+// GetUserByName request a task by name
+func (client *Client) GetUserByName(name string) (*User, error) {
+	var users, err = client.ListUsers("name=" + name)
+	if err != nil {
+		return nil, err
+	}
+	for _, user := range users {
+		if user.Username == name {
+			return &user, nil
+		}
+	}
+	return nil, errors.New("User not found")
+}
+
+// Create creates a new user
+func (user *User) Create(client *Client) (string, error) {
+	var result string
+	jsonValue, _ := json.Marshal(user)
+	body, err := client.request("POST", "users", jsonValue)
+	if err == nil {
+		result = string(body)
+	}
+	return result, err
+}
+
+//Update updates an existing user
+func (user *User) Update(client *Client) (string, error) {
+	var result string
+	jsonValue, _ := json.Marshal(user)
+	body, err := client.request("PUT", "user/"+user.ID, jsonValue)
+	if err == nil {
+		result = string(body)
+	}
+	return result, err
+}
+
+//Delete deletes a user
+func (user *User) Delete(client *Client) error {
+	if user.ID == "" {
+		return errors.New("ID cannot be empty")
+	}
+	_, err := client.request("DELETE", "user/"+user.ID, nil)
+	if err != nil {
+		return err
+	}
+	return err
+}
