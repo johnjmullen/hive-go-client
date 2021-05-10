56f978bf9b75d05a70712a3aa120a9d0afeb8277
John Mullen <john.mullen@hiveio.com>
2021-05-10 09:38:20 -0400

    refactor(debian): fix package name

--- debian/update-changelog.sh
+++ debian/update-changelog.sh
@@ -4,4 +4,4 @@ USERNAME=$(git config user.name)
 TIMESTAMP=$(date +'%a, %d %b %Y %H:%M:%S %z')
 DEBEMAIL=package@hiveio.com
 DEBFULLNAME=HiveIO
-dch --distribution unstable --package "hive-linux-agent" --newversion $DEB_VERSION "$DEB_VERSION release"
+dch --distribution unstable --package "hive-go-client" --newversion $DEB_VERSION "$DEB_VERSION release"
