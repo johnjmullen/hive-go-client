6b11e7ba4104ec8fbce300961314a3a3ebc3e520
John Mullen <john.mullen@hiveio.com>
2021-10-07 17:07:05 -0400

    chore(debian): fix jenkins builds

--- debian/update-changelog.sh
+++ debian/update-changelog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-DEB_VERSION=$(echo 8.3.0 | sed -e 's|\.\([[:digit:]]\+\)-dev\.|-\1build|g' | sed -e 's|\.\([[:digit:]]\+\)$|-\1|g')
+DEB_VERSION=$(echo 8.5.0 | sed -e 's|\.\([[:digit:]]\+\)-dev\.|-\1build|g' | sed -e 's|\.\([[:digit:]]\+\)$|-\1|g')
+DEB_VERSION="${DEB_VERSION}-$(date +%y%m%d%H)"
 USERNAME=$(git config user.name)
-TIMESTAMP=$(date +'%a, %d %b %Y %H:%M:%S %z')
 DEBEMAIL=package@hiveio.com
 DEBFULLNAME=HiveIO
 dch --distribution unstable --package "hive-go-client" --newversion $DEB_VERSION "$DEB_VERSION release"
