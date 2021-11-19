3d5476b571cb476f8712fd57d92c8c8a4fcee655
John Mullen <john.mullen@hiveio.com>
2021-11-19 17:04:48 -0500

    chore(debian): add minutes to update-changelog.sh

--- debian/update-changelog.sh
+++ debian/update-changelog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEB_VERSION=$(echo 8.5.0 | sed -e 's|\.\([[:digit:]]\+\)-dev\.|-\1build|g' | sed -e 's|\.\([[:digit:]]\+\)$|-\1|g')
-DEB_VERSION="${DEB_VERSION}-$(date +%y%m%d%H)"
+DEB_VERSION="${DEB_VERSION}-$(date +%y%m%d%H%M)"
 USERNAME=$(git config user.name)
 DEBEMAIL=package@hiveio.com
 DEBFULLNAME=HiveIO
