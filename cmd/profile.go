04e30fb342495816cd6c1eff89bb728f5ba2ee19
John Mullen <john.mullen@hiveio.com>
2022-07-12 11:24:26 -0400

    refactor(hioctl): make the profile commands consistent with the others

--- cmd/profile.go
+++ cmd/profile.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/hive-io/hive-go-client/rest"
@@ -21,110 +22,35 @@ var profileCmd = &cobra.Command{
 var profileCreateCmd = &cobra.Command{
 	Use:   "create [file]",
 	Short: "Add a new profile",
-	PreRun: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			cmd.MarkFlagRequired("name")
-		}
-		if cmd.Flags().Changed("enable-ad") {
-			cmd.MarkFlagRequired("ad-username")
-			cmd.MarkFlagRequired("ad-password")
-			cmd.MarkFlagRequired("ad-domain")
-			cmd.MarkFlagRequired("ad-user-group")
-		}
-		if cmd.Flags().Changed("enable-uv") {
-			cmd.MarkFlagRequired("uv-repository")
-		}
-	},
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		v := viper.New()
-		if len(args) == 1 {
-			var file *os.File
-			var err error
-			if args[0] == "-" {
-				file = os.Stdin
-			} else {
-				file, err = os.Open(args[0])
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
+		var file *os.File
+		var err error
+		if args[0] == "-" {
+			file = os.Stdin
+		} else {
+			file, err = os.Open(args[0])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
 			}
-			defer file.Close()
-			v.SetConfigType(viper.GetString("format"))
-			v.SetConfigFile(args[0])
-			v.ReadConfig(file)
 		}
-		v.BindPFlag("id", cmd.Flags().Lookup("id"))
-		v.BindPFlag("name", cmd.Flags().Lookup("name"))
-		v.BindPFlag("timezone", cmd.Flags().Lookup("timezone"))
-		if cmd.Flags().Changed("enable-ad") {
-			v.BindPFlag("adConfig.domain", cmd.Flags().Lookup("ad-domain"))
-			v.BindPFlag("adConfig.username", cmd.Flags().Lookup("ad-username"))
-			v.BindPFlag("adConfig.password", cmd.Flags().Lookup("ad-password"))
-			v.BindPFlag("adConfig.userGroup", cmd.Flags().Lookup("ad-user-group"))
-		}
-
-		if cmd.Flags().Changed("enable-broker-options") {
-			v.BindPFlag("brokerOptions.allowDesktopComposition", cmd.Flags().Lookup("broker-allow-desktop-composition"))
-			v.BindPFlag("brokerOptions.audioCapture", cmd.Flags().Lookup("broker-audio-capture"))
-			v.BindPFlag("brokerOptions.redirectCSSP", cmd.Flags().Lookup("broker-redirect-cssp"))
-			v.BindPFlag("brokerOptions.redirectClipboard", cmd.Flags().Lookup("broker-redirect-clipboard"))
-			v.BindPFlag("brokerOptions.redirectPNP", cmd.Flags().Lookup("broker-redirect-pnp"))
-			v.BindPFlag("brokerOptions.redirectUSB", cmd.Flags().Lookup("broker-redirect-usb"))
-			v.BindPFlag("brokerOptions.redirectPrinter", cmd.Flags().Lookup("broker-redirect-printer"))
-			v.BindPFlag("brokerOptions.redirectSmartcard", cmd.Flags().Lookup("broker-redirect-smartcard"))
-			v.BindPFlag("brokerOptions.smartResize", cmd.Flags().Lookup("broker-smart-resize"))
-			v.BindPFlag("brokerOptions.hideCertificateWarnings", cmd.Flags().Lookup("broker-hide-certificate-warnings"))
-		}
-		if cmd.Flags().Changed("enable-uv") {
-			v.BindPFlag("userVolumes.backupSchedule", cmd.Flags().Lookup("uv-backup-schedule"))
-			v.BindPFlag("userVolumes.repository", cmd.Flags().Lookup("uv-repository"))
-			v.BindPFlag("userVolumes.size", cmd.Flags().Lookup("uv-size"))
-			v.BindPFlag("userVolumes.target", cmd.Flags().Lookup("uv-target"))
-		}
+		defer file.Close()
+		data, _ := ioutil.ReadAll(file)
 		var profile rest.Profile
-		err := v.Unmarshal(&profile)
+		err = unmarshal(data, &profile)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
+
 		msg, err := profile.Create(restClient)
+		fmt.Println(msg)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println(formatString(msg))
 	},
-}
-
-func addProfileFlags(cmd *cobra.Command) {
-	cmd.Flags().StringP("id", "i", "", "profile id")
-	cmd.Flags().StringP("name", "n", "", "profile name")
-	cmd.Flags().String("timezone", "disabled", "timezone to inject")
-
-	cmd.Flags().Bool("enable-ad", false, "enable active directory options")
-	cmd.Flags().String("ad-domain", "", "AD realm")
-	cmd.Flags().String("ad-username", "", "user to join guests to AD")
-	cmd.Flags().String("ad-password", "", "Password for the join user")
-	cmd.Flags().String("ad-user-group", "", "User group")
-
-	cmd.Flags().Bool("enable-broker-options", false, "enable broker options")
-	cmd.Flags().Bool("broker-allow-desktop-composition", true, "Allow Destop compositing")
-	cmd.Flags().Bool("broker-audio-capture", true, "audio capture")
-	cmd.Flags().Bool("broker-redirect-cssp", true, "CredSSP redirection")
-	cmd.Flags().Bool("broker-redirect-clipboard", true, "Clipboard redirection")
-	cmd.Flags().Bool("broker-redirect-pnp", true, "Plug-and-Play redirection")
-	cmd.Flags().Bool("broker-redirect-usb", true, "USB redirection")
-	cmd.Flags().Bool("broker-redirect-printer", true, "Printer redirection")
-	cmd.Flags().Bool("broker-redirect-smartcard", true, "Smartcard redirection")
-	cmd.Flags().Bool("broker-smart-resize", true, "Smart screen resize")
-	cmd.Flags().Bool("broker-hide-certificate-warnings", false, "Hide certificate warnings")
-
-	cmd.Flags().Bool("enable-uv", false, "Enable user volumes")
-	cmd.Flags().Int("uv-backup-schedule", 28800, "User volume backup schedule (s)")
-	cmd.Flags().String("uv-repository", "", "Storage pool for storing the user volume")
-	cmd.Flags().Int("uv-size", 10, "User volume size (GB)")
-	cmd.Flags().String("uv-target", "disk", "Local cache (disk/ram)")
 }
 
 var profileDeleteCmd = &cobra.Command{
@@ -212,62 +138,30 @@ var profileListCmd = &cobra.Command{
 var profileUpdateCmd = &cobra.Command{
 	Use:   "update [file]",
 	Short: "update a profile",
-	PreRun: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			cmd.MarkFlagRequired("name")
-		}
-		if cmd.Flags().Changed("enable-ad") {
-			cmd.MarkFlagRequired("ad-username")
-			cmd.MarkFlagRequired("ad-password")
-			cmd.MarkFlagRequired("ad-domain")
-			cmd.MarkFlagRequired("ad-user-group")
-		}
-		if cmd.Flags().Changed("enable-uv") {
-			cmd.MarkFlagRequired("uv-repository")
-		}
-	},
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		v := viper.New()
-		if len(args) == 1 {
-			v.SetConfigFile(args[0])
-			v.ReadInConfig()
+		var file *os.File
+		var err error
+		if args[0] == "-" {
+			file = os.Stdin
+		} else {
+			file, err = os.Open(args[0])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		}
-		v.BindPFlag("id", cmd.Flags().Lookup("id"))
-		v.BindPFlag("name", cmd.Flags().Lookup("name"))
-		v.BindPFlag("timezone", cmd.Flags().Lookup("timezone"))
-		if cmd.Flags().Changed("enable-ad") {
-			v.BindPFlag("adConfig.domain", cmd.Flags().Lookup("ad-domain"))
-			v.BindPFlag("adConfig.username", cmd.Flags().Lookup("ad-username"))
-			v.BindPFlag("adConfig.password", cmd.Flags().Lookup("ad-password"))
-			v.BindPFlag("adConfig.userGroup", cmd.Flags().Lookup("ad-user-group"))
-		}
-
-		if cmd.Flags().Changed("enable-broker-options") {
-			v.BindPFlag("brokerOptions.allowDesktopComposition", cmd.Flags().Lookup("broker-allow-desktop-composition"))
-			v.BindPFlag("brokerOptions.audioCapture", cmd.Flags().Lookup("broker-audio-capture"))
-			v.BindPFlag("brokerOptions.redirectCSSP", cmd.Flags().Lookup("broker-redirect-cssp"))
-			v.BindPFlag("brokerOptions.redirectClipboard", cmd.Flags().Lookup("broker-redirect-clipboard"))
-			v.BindPFlag("brokerOptions.redirectPNP", cmd.Flags().Lookup("broker-redirect-pnp"))
-			v.BindPFlag("brokerOptions.redirectUSB", cmd.Flags().Lookup("broker-redirect-usb"))
-			v.BindPFlag("brokerOptions.redirectPrinter", cmd.Flags().Lookup("broker-redirect-printer"))
-			v.BindPFlag("brokerOptions.redirectSmartcard", cmd.Flags().Lookup("broker-redirect-smartcard"))
-			v.BindPFlag("brokerOptions.smartResize", cmd.Flags().Lookup("broker-smart-resize"))
-			v.BindPFlag("brokerOptions.hideCertificateWarnings", cmd.Flags().Lookup("broker-hide-certificate-warnings"))
-		}
-		if cmd.Flags().Changed("enable-uv") {
-			v.BindPFlag("userVolumes.backupSchedule", cmd.Flags().Lookup("uv-backup-schedule"))
-			v.BindPFlag("userVolumes.repository", cmd.Flags().Lookup("uv-repository"))
-			v.BindPFlag("userVolumes.size", cmd.Flags().Lookup("uv-size"))
-			v.BindPFlag("userVolumes.target", cmd.Flags().Lookup("uv-target"))
-		}
+		defer file.Close()
+		data, _ := ioutil.ReadAll(file)
 		var profile rest.Profile
-		err := v.Unmarshal(&profile)
+		err = unmarshal(data, &profile)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
+
 		msg, err := profile.Update(restClient)
-		fmt.Println(formatString(msg))
+		fmt.Println(msg)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -279,7 +173,6 @@ func init() {
 	RootCmd.AddCommand(profileCmd)
 
 	profileCmd.AddCommand(profileCreateCmd)
-	addProfileFlags(profileCreateCmd)
 
 	profileCmd.AddCommand(profileDeleteCmd)
 	profileDeleteCmd.Flags().StringP("id", "i", "", "profile profile Id")
@@ -293,5 +186,4 @@ func init() {
 	addListFlags(profileListCmd)
 
 	profileCmd.AddCommand(profileUpdateCmd)
-	addProfileFlags(profileUpdateCmd)
 }
