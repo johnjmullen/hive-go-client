5dbf910d1c3fc130c77e2a7c9c54c788edff9dca
John Mullen <john.mullen@hiveio.com>
2022-08-04 17:20:45 -0400

    refactor(broker): add missing broker types

--- rest/broker.go
+++ rest/broker.go
@@ -6,13 +6,33 @@ import (
 	"github.com/hashicorp/go-version"
 )
 
+//BrokerGuest describes a guest assignment
+type BrokerGuest struct {
+	Name       string `json:"name"`
+	UUID       string `json:"uuid"`
+	HostID     string `json:"hostid"`
+	GuestState string `json:"guestState"`
+	Username   string `json:"username"`
+	PoolID     string `json:"poolID"`
+	IP         string `json:"ip"`
+	UserVolume *struct {
+		BackupSchedule  interface{} `json:",omitempty"`
+		LastReplication interface{} `json:",omitempty"`
+		Source          string      `json:"source,omitempty"`
+		State           string      `json:"state,omitempty"`
+		StateMessage    interface{} `json:"stateMessage,omitempty"`
+		Target          string      `json:"Target,omitempty"`
+		RunningBackup   bool        `json:"runningBackup,omitempty"`
+	} `json:"userVolume,omitempty"`
+}
+
 //BrokerPool describes a pool received from BrokerLogin
 type BrokerPool struct {
 	ID        string        `json:"id"`
 	Name      string        `json:"name"`
 	Os        string        `json:"os"`
 	UserGroup string        `json:"userGroup"`
-	Guests    []interface{} `json:"guests"`
+	Guests    []BrokerGuest `json:"guests"`
 }
 
 type brokerLoginResponse struct {
@@ -20,42 +40,96 @@ type brokerLoginResponse struct {
 	Pools []BrokerPool `json:"pools"`
 }
 
+type BrokerConfig struct {
+	AutoConnectUserDesktop    bool   `json:"autoConnectUserDesktop"`
+	BackgroundColor           string `json:"backgroundColor"`
+	BgImage                   string `json:"bgImage"`
+	BgImageFilename           string `json:"bgImageFilename"`
+	ButtonTextColor           string `json:"buttonTextColor"`
+	ClientSourceIsolation     bool   `json:"clientSourceIsolation"`
+	Disclaimer                string `json:"disclaimer"`
+	Enabled                   bool   `json:"enabled"`
+	External                  bool   `json:"external"`
+	ExternalProfile           string `json:"externalProfile"`
+	Favicon                   string `json:"favicon"`
+	FaviconFilename           string `json:"faviconFilename"`
+	HideRealms                bool   `json:"hideRealms"`
+	HideRelease               bool   `json:"hideRelease"`
+	Logo                      string `json:"logo"`
+	LogoFilename              string `json:"logoFilename"`
+	MainColor                 string `json:"mainColor"`
+	PassthroughAuthentication bool   `json:"passthroughAuthentication"`
+	TextColor                 string `json:"textColor"`
+	Title                     string `json:"title"`
+	TwoFormAuth               struct {
+		Enabled      bool   `json:"enabled"`
+		EnforceLocal bool   `json:"enforceLocal"`
+		Type         string `json:"type"`
+	} `json:"twoFormAuth"`
+	Remote bool `json:"remote"`
+}
+
+// GetBrokerConfig returns the configuration options of the broker
+func (client *Client) GetBrokerConfig() (BrokerConfig, error) {
+	var config BrokerConfig
+	body, err := client.request("GET", "broker/configuration", nil)
+	if err != nil {
+		return config, err
+	}
+	err = json.Unmarshal(body, &config)
+	if err != nil {
+		return config, err
+	}
+	return config, err
+}
+
 // BrokerLogin connects to the broker with the provided username, password, and realm
-// returns a list of available pools for the user or an error
-func (client *Client) BrokerLogin(username, password, realm string) ([]BrokerPool, error) {
+// returns nil or an error
+func (client *Client) BrokerLogin(username, password, realm, token, mfaToken string) error {
 	jsonData := map[string]string{"username": username, "password": password, "realm": realm}
+	if token != "" {
+		jsonData["token"] = token
+	}
 	jsonValue, err := json.Marshal(jsonData)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	body, err := client.request("POST", "authBrokerUser", jsonValue)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var resp brokerLoginResponse
 	err = json.Unmarshal(body, &resp)
-	if err == nil {
-		client.token = resp.Token
+	if err != nil {
+		return err
 	}
-	return resp.Pools, err
+	client.token = resp.Token
+	return nil
+}
+
+type brokerAssignments struct {
+	Pools []BrokerPool `json:"pools"`
 }
 
 // BrokerAssignments returns an array of assignments for the logged in user
-func (client *Client) BrokerAssignments() ([]interface{}, error) {
+func (client *Client) BrokerAssignments() ([]BrokerPool, error) {
 	body, err := client.request("GET", "broker/assignments", nil)
-	var assignments []interface{}
 	if err != nil {
-		return assignments, err
+		return nil, err
 	}
+	var assignments brokerAssignments
 	err = json.Unmarshal(body, &assignments)
-	return assignments, err
+	if err != nil {
+		return nil, err
+	}
+	return assignments.Pools, err
 }
 
 // BrokerAssign requests a desktop from a pool
-func (client *Client) BrokerAssign(poolID string) (interface{}, error) {
+func (client *Client) BrokerAssign(poolID string) (BrokerGuest, error) {
 	body, err := client.request("POST", "broker/assign/"+poolID, nil)
-	var result interface{}
+	var result BrokerGuest
 	if err != nil {
 		return result, err
 	}
@@ -65,8 +139,8 @@ func (client *Client) BrokerAssign(poolID string) (interface{}, error) {
 
 // BrokerConnect request the rdp file to connect to a guest.
 // outputType can be rdp, json, or hio
-func (client *Client) BrokerConnect(guest string, remote bool, outputType string) ([]byte, error) {
-	jsonData := map[string]interface{}{"guest": guest, "remote": remote, "outputType": outputType}
+func (client *Client) BrokerConnect(guest string, outputType string) ([]byte, error) {
+	jsonData := map[string]interface{}{"guest": guest, "outputType": outputType}
 	jsonValue, err := json.Marshal(jsonData)
 	if err != nil {
 		return nil, err
