c3dd96fa8c27d4c404a232e0129e7f375e211462
John Mullen <john.mullen@hiveio.com>
2022-12-01 12:10:18 -0500

    chore(ci): fix version in update-changelog.sh

--- debian/update-changelog.sh
+++ debian/update-changelog.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DEB_VERSION=$(echo 8.5.0 | sed -e 's|\.\([[:digit:]]\+\)-dev\.|-\1build|g' | sed -e 's|\.\([[:digit:]]\+\)$|-\1|g')
-DEB_VERSION="${DEB_VERSION}-$(date +%y%m%d%H%M)"
+DEB_VERSION="8.5.0-$(date +%y%m%d%H%M)"
 USERNAME=$(git config user.name)
 DEBEMAIL=package@hiveio.com
 DEBFULLNAME=HiveIO
